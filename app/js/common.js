5fd21dabf4c130573cac9ab63e0d73fe450ebba2
Konstantin <perekonwork@yandex.ru>
2017-10-31 17:12:23 +0700

    v0.9

--- app/js/common.js
+++ app/js/common.js
@@ -4,4 +4,13 @@ $(function() {
 		$(this).toggleClass("is-active");
 	});
 
+
+	/* Main slider*/
+	// $('#js-main-slider').owlCarousel({
+	// 	loop: true,
+	// 	items: 1,
+	// 	nav:true,
+	// 	itemClass: "slider-item",
+	// })
+
 });
