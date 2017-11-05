0a49ca77053c1475dde3a9db8e00a338b81e34e7
KonstantinP <perekonwork@yandex.ru>
2017-11-05 23:53:45 +0700

    v0.17 sect stud comments ready

--- app/js/common.js
+++ app/js/common.js
@@ -13,12 +13,19 @@ $(function() {
 	// 	itemClass: "slider-item",
 	// });
 
-	/*  slider */
-	// $("#js-comment-student-slider").owlCarousel({
-	// 	loop: true,
-	// 	items: 2,
-	// 	nav: false,
-	// 	// itemClass: "slider-item",
-	// })
+	/*  Student slider */
+	$("#js-comment-student-slider").owlCarousel({
+		loop: true,
+		items: 2,
+	   responsiveClass:true,
+	   responsive:{
+	      0:{
+	         items:1,
+	      },
+	      992:{
+	         items:2
+	      }
+	   }
+	});
 
 });
