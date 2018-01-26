ee528e7a8b7974fb7e378cc93c90f87f26d967c2
Konstantin <perekonwork@yandex.ru>
2018-01-26 16:34:18 +0700

    commit ne o chem :'(

---
+++ app/libs/yandex-map/route.js
@@ -0,0 +1,39 @@
+function init () {
+    /**
+     * Создаем мультимаршрут.
+     * Первым аргументом передаем модель либо объект описания модели.
+     * Вторым аргументом передаем опции отображения мультимаршрута.
+     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml
+     */
+    var multiRoute = new ymaps.multiRouter.MultiRoute({
+        // Описание опорных точек мультимаршрута.
+        referencePoints: [
+            [55.734876, 37.59308],
+            "Москва, ул. Мясницкая"
+        ],
+        // Параметры маршрутизации.
+        params: {
+            // Ограничение на максимальное количество маршрутов, возвращаемое маршрутизатором.
+            results: 2
+        }
+    }, {
+        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+        boundsAutoApply: true
+    });
+
+
+    // Создаем карту с добавленными на нее кнопками.
+    var myMap = new ymaps.Map('js-route-map', {
+        center: [56.01839, 92.86717],
+        zoom: 13,
+        controls : ["zoomControl", "fullscreenControl"]
+    }, {
+        buttonMaxWidth: 300
+    });
+
+    // Добавляем мультимаршрут на карту.
+    myMap.geoObjects.add(multiRoute);
+}
+
+ymaps.ready(init);
