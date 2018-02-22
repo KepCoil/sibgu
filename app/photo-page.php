3460cea24879583f4953a8bafee92868e774ebed
Konstantin <perekonwork@yandex.ru>
2018-02-22 14:02:19 +0700

    photo album page ready

--- app/photo-page.php
+++ app/photo-page.php
@@ -116,6 +116,43 @@
 									<img src="img/news/news1/news_4.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="Заголовок фотки">
 								</a>
 							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/test/vertical2.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/test/vertical2.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="">
+								</a>
+							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/news/news1/news_3.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/news/news1/news_3.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="Заголовок фотки">
+								</a>
+							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/news/news1/news_4.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/news/news1/news_4.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="Заголовок фотки">
+								</a>
+							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/news/big-news/mega-news.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/news/big-news/mega-news.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="Заголовок фотки">
+								</a>
+							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/test/vertical.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/test/vertical.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="">
+								</a>
+							</div>
+
+							<div class="col-xlg-4 col-md-6 col-sm-8 photo-album-list-item">
+								<a href="img/news/news1/news_3.jpg" class="photo-album-list-item-link js-photo-gallery-init">
+									<img src="img/news/news1/news_3.jpg" class="photo-album-list-item-preview" alt="Заголовок фотки" title="Заголовок фотки">
+								</a>
+							</div>
+
 						</div>
 					</div>
 					<!-- Конец инициализация галлереи  -->
