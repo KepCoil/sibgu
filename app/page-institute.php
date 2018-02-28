21014c37916fa49be6cf8439d1b7d4c1b104eb8c
Konstantin <perekonwork@yandex.ru>
2018-02-28 09:54:13 +0700

    photo list gallery update

---
+++ app/page-institute.php
@@ -0,0 +1,37 @@
+<?php include("_head.php") ?>
+<?php include("_header.php") ?>
+<?php include("_inner-menu.php") ?>
+
+<!-- 
+	Шаблонная страница, предназначенная для выкладывания в плиточном стиле ссылки с иконками font-awesome,
+	например для "Студенту", "Ученому", "Сотруднику"
+-->
+
+<main>
+	<div id="wrap">
+		<div class="container">
+			<h2 class="inner-title">Учебные подразделения</h2>
+			<div class="row">
+
+
+				<div class="col-lg-6 col-md-8 col-sm-12">
+					<div class="wrap-standart-item">
+						<a href="#">
+							<span class="wrap-icon">
+								<span class="icis icis-kolledzh"></span>
+							</span>
+							<span class="standart-item-content">
+								<h3>Аэрокосмический колледж</h3>
+								<p>Аэрокосмический колледж создан в апреле 2008 года путем реорганизации двух учебных заведений среднего профессионального образования: Красноярского техникума космического машиностроения и Красноярского машиностроительного колледжа, вошедших в состав ФГБОУ ВО «Сибирский государственный университет науки и технологий имени академика М.Ф. Решетнёва».</p>
+							</span>
+						</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php include ("_footer.php"); ?>
+<?php include ("_end.php"); ?>
