45c06c57f865c8d8aacb29c035fe45d5fc98f4fd
perebatov konstantin <perekonwork@yandex.ru>
2018-07-26 09:25:44 +0700

    fix card personal info

--- app/__personal-info.php
+++ app/__personal-info.php
@@ -1,14 +1,14 @@
 <div class="personal-info">
 	
-	<!-- Не забыть респонсив -->
 	
 	<div class="personal-info__left">
 		<div class="personal-info__photo-wrap">
-			<img class="personal-info__photo" src="img/kustova.jpg">
+			<img class="personal-info__photo-img" src="img/kustova.jpg">
 		</div>
 	</div>
 	
-	<!-- Иконки вещать на before -->
+
+	<!-- Иконки возможно стоит вещать на before -->
 	<div class="personal-info__right">
 		<div class="personal-info__content">
 
@@ -18,29 +18,42 @@
 			<div class="personal-info__item personal-info__item--job">
 				<span>Директор Аэрокосмического колледжа</span>
 			</div>
+
+
 			<div class="personal-info__item personal-info__item--location">
-				<i class="fa fa-map-marker personal-info__icon" aria-hidden="true"></i>
-				<span>Учебный корпус "Л" , каб. 123 (г. Красноярск, ул. Мира, д.2 стр.5) </span>
+				<i class="fa fa-map-marker personal-info__item-icon" aria-hidden="true"></i>
+				<span class="personal-info__item-text">
+					Учебный корпус "Л" , каб. 123 (г. Красноярск, ул. Мира, д.2 стр.5)
+				</span>
 			</div>
 			<div class="personal-info__item personal-info__item--phone">
-				<i class="fa fa-phone personal-info__icon" aria-hidden="true"></i>
-				<span><a class="personal-info__link" href="#">+7 (3822) 426173</a> вн. т. <a class="personal-info__link" href="#">2901</a></span>
+				<i class="fa fa-phone personal-info__item-icon" aria-hidden="true"></i>
+				<span class="personal-info__item-text">
+					<a class="personal-info__item-link" href="#">+7 (3822) 426173</a>;  
+					вн. т. <a class="personal-info__item-link" href="#">2901</a>
+				</span>
 			</div>
 			<div class="personal-info__item personal-info__item--email">
-				<i class="fa fa-at personal-info__icon" aria-hidden="true"></i>
-				<span><a class="personal-info__link" href="#">webmail@sibsau.ru</a></span>
+				<i class="fa fa-at personal-info__item-icon" aria-hidden="true"></i>
+				<span class="personal-info__item-text">
+					<a class="personal-info__item-link" href="#">webmail@sibsau.ru</a>
+				</span>
 			</div>
 			<div class="personal-info__item personal-info__item--site">
-				<i class="fa fa-globe personal-info__icon" aria-hidden="true"></i>
-				<span><a class="personal-info__link" href="#" target="_blank">website.sibsau.ru</a></span>
+				<i class="fa fa-globe personal-info__item-icon" aria-hidden="true"></i>
+				<span class="personal-info__item-text">
+					<a class="personal-info__item-link" href="#" target="_blank">website.sibsau.ru</a>
+				</span>
 			</div>
-			<div class="personal-info__item personal-info__item--site">
-				<i class="fa fa-vk personal-info__icon" aria-hidden="true"></i>
-				<span><a class="personal-info__link" href="#" target="_blank">vk.com/sibsau</a></span>
-			</div>
-			
+			<div class="personal-info__item personal-info__item--soc-vk">
+				<i class="fa fa-vk personal-info__item-icon" aria-hidden="true"></i>
+				<span class="personal-info__item-text">
+					<a class="personal-info__item-link" href="#" target="_blank">vk.com/sibsau</a>
+				</span>
+			</div>	
 
 		</div>
 	</div>
+
 
 </div>
