53bd89b8d8dabada7dcb1d2467c00b7feab4d119
perebatov konstantin <perekonwork@yandex.ru>
2018-08-24 12:42:40 +0700

    project-learn update, main & list page ready

--- app/project-learn__list.php
+++ app/project-learn__list.php
@@ -18,12 +18,141 @@
 							<div class="title-border">
 								<h2 class="inner-title">Проектная деятельность. Институт информатики и телекоммуникаций</h2>
 							</div>
-							
+
+							<div class="project-learn-list">
+
+								<div id="accordion" class="panel-group main-accordion main-accordion--bg-dis" role="tablist">
+									<div class="panel accordion-item">
+										<div class="panel-heading" role="tab">
+											<h4 class="panel-title">
+												<i class="fa fa-hand-o-right" aria-hidden="true"></i>
+												<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#id-1" aria-expanded="false">
+													15.03.04 Автоматизация технологических процессов и производств
+												</a>
+											</h4>
+										</div>
+										<div id="id-1" class="panel-collapse collapse" aria-expanded="false" role="tabpanel">
+											<div class="panel-body">
+												<div class="table-responsive">
+													<table class="table table-striped table-patents">
+														<thead>
+															<tr>
+																<th>Проект</th>
+																<th class="fix-width-250">Руководитель</th>
+																<th class="fix-width-250">Заказчик</th>
+															</tr>
+														</thead>
+
+														<tbody>
+															<tr>
+																<td><a href="#">Усилитель-концентратор пучка электронов с электронной мембраной</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+															<tr>
+																<td><a href="#">Электрический имитатор аккумуляторной батареи с защитой по току и напряжению и устройство защиты электрического имитатора аккумуляторной батареи</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+															<tr>
+																<td><a href="#">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Продолжил, осталось свой жизни подпоясал!</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+															<tr>
+																<td><a href="#">Далеко-далеко за словесными горами в стране, гласных и согласных живут.</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
 
 
+									<div class="panel accordion-item">
+										<div class="panel-heading" role="tab">
+											<h4 class="panel-title">
+												<i class="fa fa-hand-o-right" aria-hidden="true"></i>
+												<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#id-2" aria-expanded="false">
+													22.03.01 Материаловедение и технологии материалов
+												</a>
+											</h4>
+										</div>
+										<div id="id-2" class="panel-collapse collapse" role="tabpanel">
+											<div class="panel-body">
+												<div class="table-responsive">
+													<table class="table table-striped table-patents">
+														<thead>
+															<tr>
+																<th>Проект</th>
+																<th class="fix-width-250">Руководитель </th>
+																<th class="fix-width-250">Заказчик</th>
+															</tr>
+														</thead>
+
+														<tbody>
+															<tr>
+																<td><a href="#">Усилитель-концентратор пучка электронов с электронной мембраной</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+															<tr>
+																<td><a href="#">Электрический имитатор аккумуляторной батареи с защитой по току и напряжению и устройство защиты электрического имитатора аккумуляторной батареи</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
+
+
+									<div class="panel accordion-item">
+										<div class="panel-heading" role="tab">
+											<h4 class="panel-title">
+												<i class="fa fa-hand-o-right" aria-hidden="true"></i>
+												<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#id-3" aria-expanded="false">
+													35.03.02 Технология лесозаготовительных и деревоперерабатывающих производств
+												</a>
+											</h4>
+										</div>
+										<div id="id-3" class="panel-collapse collapse" role="tabpanel">
+											<div class="panel-body">
+												<div class="table-responsive">
+													<table class="table table-striped table-patents">
+														<thead>
+															<tr>
+																<th>Проект</th>
+																<th class="fix-width-250">Руководитель </th>
+																<th class="fix-width-250">Заказчик</th>
+															</tr>
+														</thead>
+
+														<tbody>
+															<tr>
+																<td><a href="#">Усилитель-концентратор пучка электронов с электронной мембраной</a></td>
+																<td>Руководитель</td>
+																<td>Заказчик</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
+
+								</div>
+
+							</div>
 						</div>
-						
 					</div>
+
+
 
 					<div id="js-sticky-sidebar" class="col-lg-5 col-lg-pull-19 col-md-6 col-md-pull-18 page-sidebars__colums">
 						<div class="page-sidebars__sidebar">
--- app/project-learn__main.php
+++ app/project-learn__main.php
@@ -20,7 +20,7 @@
 							<a href="#project-learn-info" aria-controls="home" role="tab" data-toggle="tab">Информация</a>
 						</li>
 						<li role="presentation">
-							<a href="#project-learn-students" aria-controls="profile" role="tab" data-toggle="tab">Обущающемуся</a>
+							<a href="#project-learn-students" aria-controls="profile" role="tab" data-toggle="tab">Студенту</a>
 						</li>
 						<li role="presentation">
 							<a href="#project-learn-clients" aria-controls="profile" role="tab" data-toggle="tab">Заказчику</a>
@@ -32,6 +32,51 @@
 						<div role="tabpanel" class="tab-pane fade in active" id="project-learn-info">
 							<div class="project-learn-page__tab-item">
 								<h3>Общая информация о проектной деятельности</h3>
+
+								<div class="personal-info">
+
+									<div class="personal-info__left">
+										<div class="personal-info__photo-wrap">
+											<img class="personal-info__photo-img" src="img/sibgu_no_photo.png" />
+										</div>
+									</div>
+
+									<div class="personal-info__right">
+										<div class="personal-info__content">
+
+
+											<div class="personal-info__item personal-info__item--name">
+												<span class="personal-info__item-text">Соколов Владислав Львович</span>
+											</div>
+											<div class="personal-info__item personal-info__item--job">
+												<span class="personal-info__item-text">Начальник отдела основных образовательных программ и современных образовательных технологий</span>
+											</div>
+
+
+											<div class="personal-info__item personal-info__item--location">
+												<i class="fa fa-map-marker personal-info__item-icon" aria-hidden="true"></i>
+												<span class="personal-info__item-text">
+													Корпус «Цл», проспект Мира, 82, каб. Цл-201
+												</span>
+											</div>
+											<div class="personal-info__item personal-info__item--phone">
+												<a class="personal-info__item-link" href="tel:227-51-60">
+													<i class="fa fa-phone personal-info__item-icon" aria-hidden="true"></i>	
+													<span class="personal-info__item-text">227-51-60</span>
+												</a>
+											</div>
+
+											<div class="personal-info__item personal-info__item--email">
+												<a class="personal-info__item-link" href="mailto:sokolov@sibsau.ru">
+													<i class="fa fa-at personal-info__item-icon" aria-hidden="true"></i>
+													<span class="personal-info__item-text">sokolov@sibsau.ru</span>
+												</a>
+											</div>
+
+										</div>
+									</div>
+								</div>
+
 								<p><strong>Проектная деятельность</strong> - это ...</p>
 								<br/>
 								<h5 class="project-learn-page__local-title">Цели проектной деятельности</h5>
@@ -40,13 +85,14 @@
 									<li>Цель 2</li>
 									<li>Цель 3</li>
 								</ul>
+								<p><a href="#" class="doc-file doc-file--pdf">Положение об организации и осуществлении проектной деятельности</a></p>
 							</div>
 						</div>
 
 
 						<div role="tabpanel" class="tab-pane fade" id="project-learn-students">
 							<div class="project-learn-page__tab-item project-learn-page__tab-item--learn-department">
-								<h3>Обущающемуся</h3>
+								<h3>Студенту</h3>
 
 								<h5 class="project-learn-page__local-title">Институты</h5>
 								<ul class="ul-accent project-learn-page__local-list">
